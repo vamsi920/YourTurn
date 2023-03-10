dd8f6f061ab3245a5cd82f993855d88f065f6e3c
vamsi920 <vammu920@gmail.com>
2023-03-10 16:19:53 -0500

    Initial Commit

---
+++ Yourturn/YourturnApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourturnApp.swift
+//  Yourturn
+//
+//  Created by Venkata Vamsi Devadula on 3/10/23.
+//
+
+import SwiftUI
+
+@main
+struct YourturnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
